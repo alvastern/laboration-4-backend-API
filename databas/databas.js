c483533953e62962038b9867750f8ddfba58e47e
alvastern <alva.stern02@gmail.com>
2026-05-07 09:25:45 +0200

    Skapade en SQLite-databas och en tabell i databas.js för denna.

---
+++ databas/databas.js
@@ -0,0 +1,13 @@
+const sqlite = require('sqlite3').verbose();
+const database = new sqlite.Database('.databas/databas.db');
+
+database.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`);
+
+module.exports = database;
