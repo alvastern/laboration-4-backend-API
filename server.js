0b669cbe754629fc36bb104ca8e5dbce99243eac
alvastern <alva.stern02@gmail.com>
2026-05-10 16:44:07 +0200

    Lade till cors som require

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const routes = require('./routes/routes.js');
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
